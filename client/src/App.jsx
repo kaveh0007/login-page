82d13df0e95deead343c9a608632a5feb97a0c1e
kaveh0007 <156835611+kaveh0007@users.noreply.github.com>
2025-06-19 13:13:58 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     const password = passwordInput.value;
 
     try {
-      const res = await fetch("https://login-page-2asm.onrender.com", {
+      const res = await fetch(`${BASE_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ emailId, password }),
@@ -54,7 +54,7 @@ function App() {
     }
 
     try {
-      const res = await fetch("https://login-page-2asm.onrender.com", {
+      const res = await fetch(`${BASE_URL}/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ emailId: email, password }),
